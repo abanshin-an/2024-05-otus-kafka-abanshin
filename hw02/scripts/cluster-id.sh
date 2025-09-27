b3595a73421ae2c6452517f5e509cd4ffef8b8b8
andrewabanshin <a2n9@mail.ru>
2025-09-27 14:25:39 +0400

    Revert "Homework 2"
    This reverts commit 7a49a8591dc8eca2afe3c5aab329e1835d01f69f.

--- hw02/scripts/cluster-id.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CLUSTER_ID=$(openssl rand -base64 16 | tr '+/' '-_' | tr -d '=' | cut -c1-22)
-
-sed -i.bak "s/^CLUSTER_ID=.*$/CLUSTER_ID=$CLUSTER_ID/" .env
-rm .env.bak
-echo "Updated CLUSTER_ID in .env to $CLUSTER_ID"
--- hw02/scripts/set-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-KAFKA_CFG=$1
-sed -i.bak "s/^KAFKA_CFG=.*$/KAFKA_CFG=$KAFKA_CFG/" .env
-rm .env.bak
-echo "Updated KAFKA_CFG in .env to $KAFKA_CFG"
