4bdaea0757cbdf097ba976bb444954881da35664
andrewabanshin <a2n9@mail.ru>
2025-10-14 02:20:50 +0400

    Homework 3

--- hw03/scripts/create_topics.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source .env
-#docker exec kafka-hw03 kafka-topics --bootstrap-server $BOOTSTRAP_SERVER --command-config $ADMIN_CONFIG --create --topic topic1
-#docker exec kafka-hw03 kafka-topics --bootstrap-server $BOOTSTRAP_SERVER --command-config $ADMIN_CONFIG --create --topic topic2
-#
-#docker exec kafka-hw03 kafka-topics --bootstrap-server $BOOTSTRAP_SERVER --command-config $ADMIN_CONFIG --list
-#docker exec kafka-hw03 kafka-acls   --bootstrap-server $BOOTSTRAP_SERVER --command-config $ADMIN_CONFIG --list
